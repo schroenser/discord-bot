a2918ce4150546e6ee956b115b2a0a23aa9de82b
Sven Haberer <schroenser@gmail.com>
2019-07-08 22:58:06 +0200

    Fixed line breaks in conjunction with formatting.

--- src/main/java/de/schroenser/discord/TicketCounter.java
+++ src/main/java/de/schroenser/discord/TicketCounter.java
@@ -141,7 +141,7 @@ public class TicketCounter extends ListenerAdapter
             {
                 result += "**";
             }
-            result += (i + 1) + ". " + waitingMember.getMember().getEffectiveName() + "\n";
+            result += (i + 1) + ". " + waitingMember.getMember().getEffectiveName();
             if (waitingMember.getMustered() != null)
             {
                 result += "**";
@@ -150,6 +150,7 @@ public class TicketCounter extends ListenerAdapter
             {
                 result += "~~";
             }
+            result += "\n";
         }
 
         message.setText(result);
